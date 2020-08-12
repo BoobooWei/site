d6927a459c4f16982629d13486b212cdf826578f
booboowei <rgweiyaping@hotmail.com>
2020-08-12 09:51:03 +0800

    add phpems

--- source/oracle/oracle-11g/scripts/login.sql
+++
@@ -1,8 +0,0 @@
-define _editor=vi
-set serveroutput on size 1000000
-set trimspool on
-set long 5000
-set linesize 100
-set pagesize 9999
-column plan_plus_exp format a80
-set sqlprompt '&_user.@&_connect_identifier.>'
